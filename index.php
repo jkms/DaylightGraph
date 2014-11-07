d8ae8652ac79fa92029d1330cff1d6740ce5057b
John <john@jkms.me>
2014-11-07 09:12:14 -0800

    revert!

--- index.php
+++ index.php
@@ -129,9 +129,8 @@ function DrawGraph($line1, $color1, $line2, $color2, $line3, $color3, $line4, $c
 	for ($i=1; $i<=$width; $i++) {
 		echo "\n		context.lineTo(".$coord[0]['x'][$i].", ".$coord[0]['y'][$i].");";
 	}
-	echo "\n		context.closepath();
+	echo "\n		context.lineJoin = 'round';
 		context.lineWidth = 2;
-		context.fillStyle = '#8ED6FF';
 		context.strokeStyle = '$color1';
 		context.stroke();";
 		
