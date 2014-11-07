d41022c90c34c6d77ff577c72fe0be417312f49d
John <john@jkms.me>
2014-11-06 22:56:59 -0800

    bugfix

--- index.php
+++ index.php
@@ -127,7 +127,7 @@ function DrawGraph($line1, $color1, $line2, $color2, $line3, $color3, $line4, $c
 		context.moveTo(".$coord[0]['x'][0].", ".$coord[0]['y'][0].");";
 	$width += 3;
 	for ($i=1; $i<=$width; $i++) {
-		echo "\n		context.lineTo(".$coord[0]['x'][$i].", ".$coord[0]['y'][$i].");";
+		echo "/n		context.lineTo(".$coord[0]['x'][$i].", ".$coord[0]['y'][$i].");";
 	}
 	echo "\n		context.closepath();
 //		context.lineJoin = 'round';
@@ -149,7 +149,7 @@ function DrawGraph($line1, $color1, $line2, $color2, $line3, $color3, $line4, $c
 		context.moveTo(".$coord[1]['x'][0].", ".$coord[1]['y'][0].");";
 
 	for ($i=1; $i<=$width; $i++) {
-		echo "\n		context.lineTo(".$coord[1]['x'][$i].", ".$coord[1]['y'][$i].");";
+		echo "/n		context.lineTo(".$coord[1]['x'][$i].", ".$coord[1]['y'][$i].");";
 	}
 	echo "\n		context.lineJoin = 'round';
 		context.lineWidth = 2;
