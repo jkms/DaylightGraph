787c0e6169d182b48d8bab776f984e42076cd151
John <john@jkms.me>
2014-11-06 17:06:39 -0800

    less grid lines

--- index.php
+++ index.php
@@ -50,13 +50,13 @@ function DrawGraph($line1, $color1, $line2, $color2, $line3, $color3, $line4, $c
 		
 	//Grid
 		//Vertical
-		for ($i=1; $i<=12; $i++) {
+		for ($i=1; $i<12; $i++) {
 			$coord[1000]['x'][0][] = ($i/12) * $graph['x'];
 			$coord[1000]['y'][0][] = $graph['x'] * 0;
 			$coord[1000]['x'][1][] = ($i/12) * $graph['x'];
 			$coord[1000]['y'][1][] = $graph['x'] * 1;
 		}
-		for ($i=0; $i<=11; $i++) {
+		for ($i=0; $i<11; $i++) {
 			echo "\n		context.beginPath();
 			context.moveTo(".$coord[1000]['x'][0][$i].", ".$coord[1000]['y'][0][$i].");
 			context.lineTo(".$coord[1000]['x'][1][$i].", ".$coord[1000]['y'][1][$i].");
