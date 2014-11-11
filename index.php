bc91dfebe8356c6ae5c1b771fa90bffffbf4641f
John <john@jkms.me>
2014-11-11 15:23:15 -0800

    stab in the dark

--- index.php
+++ index.php
@@ -32,8 +32,8 @@ function loadRunKeeper ($file) {
 $filename = "Data/johnrun/2014-01-01-0951.gpx";
 
     echo "<h1>$filename</h1>\n";
-    $gpx = loadRunKeeper($filename);
-    print_r($gpx);
+    $trkseg = loadRunKeeper($filename);
+    print_r($trkseg);
 //}
 
 $start = date('z') * -1;
